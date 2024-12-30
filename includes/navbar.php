80f5272f854b9cbe9d1d5c36c568e84484b2e399
narmatha01palani <narmathapalani01@gmail.com>
2024-12-30 22:31:17 +0530

    first commit

---
+++ includes/navbar.php
@@ -0,0 +1,63 @@
+<header class="main-header">
+  <nav class="navbar navbar-static-top">
+    <div class="container">
+      <div class="navbar-header">
+        <a href="index.php" class="navbar-brand"><b>Ecommerce</b>Site</a>
+        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse">
+          <i class="fa fa-bars"></i>
+        </button>
+      </div>
+
+      <!-- Collect the nav links, forms, and other content for toggling -->
+      <div class="collapse navbar-collapse pull-left" id="navbar-collapse">
+        <ul class="nav navbar-nav">
+          <li><a href="index.php">HOME</a></li>
+          <li class="dropdown">
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown">CATEGORY <span class="caret"></span></a>
+            <ul class="dropdown-menu" role="menu">
+              <?php
+             
+                $conn = $pdo->open();
+                try{
+                  $stmt = $conn->prepare("SELECT * FROM category");
+                  $stmt->execute();
+                  foreach($stmt as $row){
+                    echo "
+                      <li><a href='category.php?category=".$row['cat_slug']."'>".$row['name']."</a></li>
+                    ";                  
+                  }
+                }
+                catch(PDOException $e){
+                  echo "There is some problem in connection: " . $e->getMessage();
+                }
+
+                $pdo->close();
+
+              ?>
+            </ul>
+          </li>
+        </ul>
+      </div>
+      <div class="navbar-custom-menu">
+        <ul class="nav navbar-nav">
+          <li class="dropdown messages-menu">
+            <!-- Menu toggle button -->
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+              <i class="fa fa-shopping-cart"></i>
+              <span class="label label-success cart_count"></span>
+            </a>
+            <ul class="dropdown-menu">
+              <li class="header">You have <span class="cart_count"></span> item(s) in cart</li>
+              <li>
+                <ul class="menu" id="cart_menu">
+                </ul>
+              </li>
+              <li class="footer"><a href="cart_view.php">Go to Cart</a></li>
+            </ul>
+          </li>
+
+        </ul>
+      </div>
+    </div>
+  </nav>
+</header>
